ead70baded33a6de600a322970338f7915eb6045
arjunq21 <arjunadhikari789@gmail.com>
2023-01-23 15:48:34 +0545

    Added functions

--- dart-basics/functions.dart
+++ dart-basics/functions.dart
@@ -2,19 +2,30 @@ String giveMeHi() {
   return "Hi";
 }
 
-void main() {
-  print(giveMeHi());
-  print("The sum of 5 and 6 is: ${add(5, 6)}");
-
-  print("The sum of 5 and 6, with named arguments is: ${add2(a: 5, b: 6)}");
-
-  print("The sum of 5 and 6, with optional arguments is: ${addOptional(b: 6)}");
-
-  print("The sum of 5 and 6, with both arguments is: ${addBoth(5, c: 6)}");
-}
-
 double add(double c, double b) {
   return c + b;
+}
+
+typedef hamroFunctionType = double? Function(double, double);
+void runAddition(hamroFunctionType function) {
+  print(function(5, 4));
+}
+
+hamroFunctionType getAdditionFunction() {
+  return add;
+}
+
+void main() {
+  runAddition(getAdditionFunction());
+
+  // print(giveMeHi());
+  // print("The sum of 5 and 6 is: ${add(5, 6)}");
+
+  // print("The sum of 5 and 6, with named arguments is: ${add2(a: 5, b: 6)}");
+
+  // print("The sum of 5 and 6, with optional arguments is: ${addOptional(b: 6)}");
+
+  // print("The sum of 5 and 6, with both arguments is: ${addBoth(5, c: 6)}");
 }
 
 double add2({required double a, required double b}) {
@@ -41,7 +52,5 @@ double addBoth(double a, {double? c}) {
 // return a + (c ?? 0);
 
 // }
-
-
 
 
