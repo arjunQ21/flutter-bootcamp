3fb8986026b616f67786cb6e602af3b728d19554
arjunq21 <arjunadhikari789@gmail.com>
2023-02-28 12:03:50 +0545

    Pulling data with async await

---
+++ day_18/lib/models/post.dart
@@ -0,0 +1,22 @@
+class Post {
+  int userId;
+  int id;
+  String title;
+  String body;
+
+  Post({
+    required this.userId,
+    required this.id,
+    required this.title,
+    required this.body,
+  });
+
+  static Post fromJson(Map<String, dynamic> json) {
+    return Post(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+    );
+  }
+}
---
+++ day_18/lib/with_async_await.dart
@@ -0,0 +1,65 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import "package:http/http.dart" as http;
+
+import 'models/post.dart';
+
+class PullWithAsyncAwait extends StatefulWidget {
+  const PullWithAsyncAwait({super.key});
+
+  @override
+  State<PullWithAsyncAwait> createState() => _PullWithAsyncAwaitState();
+}
+
+class _PullWithAsyncAwaitState extends State<PullWithAsyncAwait> {
+  List<Post> posts = [];
+
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  Future<void> fetchData() async {
+    try {
+      setState(() {
+        _isLoading = true;
+      });
+      final response = await http
+          .get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+      List<dynamic> jsonDecoded = jsonDecode(response.body);
+      posts = jsonDecoded.map((e) => Post.fromJson(e)).toList();
+    } catch (e) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text(e.toString())));
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Pull with async/await")),
+      body: _isLoading
+          ? Center(
+              child: CupertinoActivityIndicator(
+              radius: 20,
+            ))
+          : ListView.builder(
+              itemCount: posts.length,
+              itemBuilder: ((context, index) => ListTile(
+                    title: Text(posts[index].title),
+                    subtitle: Text(posts[index].body),
+                  )),
+            ),
+    );
+  }
+}
