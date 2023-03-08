1460d20272d8d97ebeb4a6329a39746fcdd4427a
arjunq21 <arjunadhikari789@gmail.com>
2023-03-08 13:25:01 +0545

    Added getInvolvedVotings() function

--- voting_system/lib/screens/system/result_screen.dart
+++ voting_system/lib/screens/system/result_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:jsend/jsend.dart';
+
+import '../../models/voting.dart';
 
 class ResultScreen extends StatefulWidget {
   const ResultScreen({Key? key}) : super(key: key);
@@ -8,10 +11,41 @@ class ResultScreen extends StatefulWidget {
 }
 
 class _ResultScreenState extends State<ResultScreen> {
+
+Future<List<Voting>> getInvolvedVotings()async {
+
+var response = await jsendResponse.fromAPIRequest(APIRequest(
+path: "/users/me/votings",
+method: "GET",
+)) ;
+
+if(response.status == 'success'){
+
+  var rawVotings = response.data['votings'] as List ;
+
+  List<Voting> parsedVotings = [] ;
+
+  for(var rawVoting in rawVotings){
+
+    parsedVotings.add(Voting.fromJson(rawVoting)) ;
+
+  }
+
+  return parsedVotings ;
+
+}else{
+  throw Exception("Failed to fetch votings: ${response.message}") ;
+}
+
+}
+
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Result Screen'),
-    );
+    return FutureBuilder( future: 
+    
+    
+    
+    , builder: ((context, snapshot) {}));
   }
 }
