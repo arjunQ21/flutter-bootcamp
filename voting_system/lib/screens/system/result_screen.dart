4d5ad7a1f50b7360ede2fc2290bdc6e6155c0711
arjunq21 <arjunadhikari789@gmail.com>
2023-03-08 14:09:05 +0545

    Added raw chart

--- voting_system/lib/screens/system/result_screen.dart
+++ voting_system/lib/screens/system/result_screen.dart
@@ -1,3 +1,5 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:jsend/jsend.dart';
 
@@ -11,41 +13,93 @@ class ResultScreen extends StatefulWidget {
 }
 
 class _ResultScreenState extends State<ResultScreen> {
+  int touchedIndex = 0;
 
-Future<List<Voting>> getInvolvedVotings()async {
+  Future<List<Voting>> getInvolvedVotings() async {
+    var response = await jsendResponse.fromAPIRequest(APIRequest(
+      path: "/users/me/votings",
+      method: "GET",
+    ));
 
-var response = await jsendResponse.fromAPIRequest(APIRequest(
-path: "/users/me/votings",
-method: "GET",
-)) ;
+    if (response.status == 'success') {
+      var rawVotings = response.data['votings'] as List;
 
-if(response.status == 'success'){
+      List<Voting> parsedVotings = [];
 
-  var rawVotings = response.data['votings'] as List ;
+      for (var rawVoting in rawVotings) {
+        parsedVotings.add(Voting.fromJson(rawVoting));
+      }
 
-  List<Voting> parsedVotings = [] ;
-
-  for(var rawVoting in rawVotings){
-
-    parsedVotings.add(Voting.fromJson(rawVoting)) ;
-
+      return parsedVotings;
+    } else {
+      throw Exception("Failed to fetch votings: ${response.message}");
+    }
   }
-
-  return parsedVotings ;
-
-}else{
-  throw Exception("Failed to fetch votings: ${response.message}") ;
-}
-
-}
-
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder( future: 
-    
-    
-    
-    , builder: ((context, snapshot) {}));
+    return FutureBuilder(
+      future: getInvolvedVotings(),
+      builder: ((context, snapshot) {
+        if (snapshot.hasError) {
+          return Center(child: Text("Error: ${snapshot.error}"));
+        } else if (snapshot.hasData) {
+          var resultVotings = snapshot.data as List<Voting>;
+          return SingleChildScrollView(
+            child: Column(
+              // ignore: unnecessary_cast
+              children: (resultVotings
+                  .map(
+                    (rv) => SizedBox(
+                      height: 500,
+                      child: PieChart(
+                        PieChartData(
+                          pieTouchData: PieTouchData(
+                            touchCallback:
+                                (FlTouchEvent event, pieTouchResponse) {
+                              setState(() {
+                                if (!event.isInterestedForInteractions ||
+                                    pieTouchResponse == null ||
+                                    pieTouchResponse.touchedSection == null) {
+                                  touchedIndex = -1;
+                                  return;
+                                }
+                                touchedIndex = pieTouchResponse
+                                    .touchedSection!.touchedSectionIndex;
+                              });
+                            },
+                          ),
+                          borderData: FlBorderData(
+                            show: false,
+                          ),
+                          sectionsSpace: 5,
+                          centerSpaceRadius: 40,
+                          // ignore: unnecessary_cast
+                          sections: (rv.candidates
+                              .map((c) => PieChartSectionData(
+                                    color: Colors.blue,
+                                    value: c.votes.toDouble(),
+                                    title: c.name,
+                                    radius: 45,
+                                    titleStyle: TextStyle(
+                                      fontSize: 17,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.red,
+                                    ),
+                                    badgeWidget: FlutterLogo(),
+                                    badgePositionPercentageOffset: .98,
+                                  ))
+                              .toList() as List<PieChartSectionData>),
+                        ),
+                      ),
+                    ),
+                  )
+                  .toList() as List<Widget>),
+            ),
+          );
+        }
+        return Center(child: CupertinoActivityIndicator());
+      }),
+    );
   }
 }
